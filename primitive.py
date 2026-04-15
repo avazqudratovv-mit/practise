d55ad3048fe47f3fcb3ab377c219966bee8dcf44
Kenneth <avazqudratovv@gmail.com>
2026-04-15 15:00:59 -0700

    feat: learn string in Python

--- primitive.py
+++ primitive.py
@@ -1,4 +1,4 @@
-print("==============")
+print("====== number ========")
 # in JAVA, Variable is a name of storage location !
 # in Python, variable is named reference !
 
@@ -9,3 +9,20 @@ print(f"the count: {count} and type: {count_type}")
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state
 print(result1, result2)
+
+print("====== string ========")
+# METHODS: upper() lower() title() find() replace()
+
+course = "AI Python Fullstack"
+result = type(course)
+print(f"the result (1): {result}")
+
+result = course.title()
+print(f"the result (2): {result}")
+
+result = course.upper()
+print(f"the result (3): {result}")
+
+result = course.replace("Fullstack", "Masterclass")
+print(f"the result (3): {result}")
+print(course)
