0f9d26b12ac60b9f86194126b676dbe777ceb8f5
Kenneth <avazqudratovv@gmail.com>
2026-04-19 18:16:23 -0700

    fix: python special/magic methods

--- class.about.py
+++ class.about.py
@@ -1,11 +1,11 @@
 ''' CLASS
-     (1) What is class
-     (2) Ordinary vs static properties 
-     (3) Special methods
+     (1) What is Class
+     (2) ordinary vs static properties
+     (3) special/magic methods
 '''
 
 print("===== What is class =====")
-# class - blueprint for object creation
+# class - blueprint for object creation!
 # structure > state constructor method
 
 
@@ -20,15 +20,14 @@ class Person():
 
     # method
     def introduce(self):
-        print(f"the {self.name} says: How do you do!")
+        print(f"{self.name} says: How do you do!")
 
     def say_age(self):
         print(f"{self.name} says I am {self.age}!")
 
-
-@classmethod
-def explain(cls):
-    print("Class: static method property executed")
+    @classmethod
+    def explain(cls):
+        print("static method property executed!")
 
 
 person1 = Person("Justin", 25)
@@ -36,17 +35,61 @@ person2 = Person("Martin", 35)
 person3 = Person("John", 22)
 
 # ordinary state
-print("person.1name:", person1.name)
+print("person1.name:", person1.name)
 
 # ordinary method
 person1.introduce()
 person2.say_age()
 
 
-print("===== Ordinary vs static properties  =====")
+print("===== ordinary vs static properties")
 # static state
 new_message = Person.message
 print("new_message:", new_message)
 
-# statoc method
-Person.explain()
+# static method
+Person.explain
+
+
+print("===== special/magic methods =====")
+# Python's most common special methods are below
+# __init__ __new__ __str__ __call__ __getitem__ __eq__ __len__ ...
+
+
+class Car():
+    # state
+    description = "This class makes cars"
+
+    # constructor
+    def __new__(cls, *args):
+        print("*__new__*")
+        return super().__new__(cls)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    # method
+    def start_engine(self):
+        print(f"the {self.name} started engine!")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped engine!")
+
+    def __str__(self):
+        return f"{self.name} was produced in {self.year} year !"
+
+    def __call__(self):
+        print("Object called as function !")
+        return True
+
+
+my_car = Car("Ferrari", 2025)
+my_car.start_engine()
+my_car.stop_engine()
+
+print("------")
+your_car = Car("Tayota", 2026)
+print(your_car)
+response = your_car()  # CALL
+print("response:", response)
